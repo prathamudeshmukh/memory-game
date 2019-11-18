fa7b1a39011eab30c52e4683848003a5124ffd92
Administrator <prathamudeshmukh@gmail.com>
2019-11-18 09:35:54 +0530

    Updates

---
+++ precache-manifest.0163dc10562e36b97d4a307a11ee7d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d2479ad933d4426846c8b6b1ab8eb1",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "779e559c60955b07db5e",
+    "url": "/memory-game/static/css/2.53dc0e97.chunk.css"
+  },
+  {
+    "revision": "5c77ad9d328a0c3b0c11",
+    "url": "/memory-game/static/css/main.d1d8fdea.chunk.css"
+  },
+  {
+    "revision": "779e559c60955b07db5e",
+    "url": "/memory-game/static/js/2.d532c1b2.chunk.js"
+  },
+  {
+    "revision": "5c77ad9d328a0c3b0c11",
+    "url": "/memory-game/static/js/main.dc32d44d.chunk.js"
+  },
+  {
+    "revision": "3c6c1d3098f1f9b94d8d",
+    "url": "/memory-game/static/js/runtime-main.53e70fbf.js"
+  }
+]);
